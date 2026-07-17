59697fbbcf539db2f49538168604e1bbbe739ec1
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-07-17 13:38:04 +0300

    feat: create functions

--- Working_with_Queries_in_Django_Exercise_29_06_2026/caller.py
+++ Working_with_Queries_in_Django_Exercise_29_06_2026/caller.py
@@ -9,7 +9,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 from main_app.models import ArtworkGallery, ChessPlayer, Meal, Dungeon, Workout, Laptop
-from main_app.choices import LaptopChoices, OSChoices
+from main_app.choices import LaptopChoices, OSChoices, MealTypeChoices
 from django.db.models import QuerySet, Q, F, Case, When, Value
 from django.db import connection, reset_queries
 from pprint import pp
@@ -131,3 +131,79 @@ def update_operation_systems() -> None:
 
 def delete_inexpensive_laptops() -> None:
     Laptop.objects.filter(price__lt=1200).delete()
+
+
+
+def bulk_create_chess_players(args: List[ChessPlayer]) -> None:
+    ChessPlayer.objects.bulk_create(args)
+
+def delete_chess_players() -> None:
+    ChessPlayer.objects.filter(title="no title").delete()
+
+def change_chess_games_won() -> None:
+    """
+    UPDATE
+        chess_player
+    SET
+        games_won = 30
+    WHERE
+    title = "GM";
+    """
+    ChessPlayer.objects.filter(title='GM').update(games_won=30)
+
+def change_chess_games_lost() -> None:
+    ChessPlayer.objects.filter(title="no title").update(games_lost=25)
+
+def change_chess_games_drawn() -> None:
+    ChessPlayer.objects.update(games_drawn=10)
+
+def grand_chess_title_gm() -> None:
+    ChessPlayer.objects.filter(rating__gte=2400).update(title="GM")
+
+def grand_chess_title_im() -> None:
+    """
+    UPDATE
+        chess_player
+    SET
+        title = "IM"
+    WHERE
+        rating BETWEEN 2300 AND 2399;
+    """
+    ChessPlayer.objects.filter(rating__range=[2300, 2399]).update(title="IM")
+
+def grand_chess_title_fm() -> None:
+    ChessPlayer.objects.filter(rating__range=[2200, 2299]).update(title="FM")
+
+def grand_chess_title_regular_player() -> None:
+    ChessPlayer.objects.filter(rating__range=[0, 2199]).update(title="regular player")
+
+
+
+def set_new_chefs() -> None:
+    Meal.objects.update(
+        chef=Case(
+            When(meal_type=MealTypeChoices.BREAKFAST, then=Value("Gordon Ramsay")),
+            When(meal_type=MealTypeChoices.LUNCH, then=Value("Julia Child")),
+            When(meal_type=MealTypeChoices.DINNER, then=Value("Jamie Oliver")),
+            When(meal_type=MealTypeChoices.SNACK, then=Value("Thomas Keller")),
+        ),
+    )
+
+def set_new_preparation_times() -> None:
+    Meal.objects.update(
+        preparation_time=Case(
+            When(meal_type=MealTypeChoices.BREAKFAST, then=Value("10 minutes")),
+            When(meal_type=MealTypeChoices.LUNCH, then=Value("12 minutes")),
+            When(meal_type=MealTypeChoices.DINNER, then=Value("15 minutes")),
+            When(meal_type=MealTypeChoices.SNACK, then=Value("5 minutes")),
+        )
+    )
+
+def update_low_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=[MealTypeChoices.BREAKFAST, MealTypeChoices.DINNER]).update(calories=400)
+
+def update_high_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=[MealTypeChoices.LUNCH, MealTypeChoices.SNACK]).update(calories=700)
+
+def delete_lunch_and_snack_meals() -> None:
+    Meal.objects.filter(meal_type__in=[MealTypeChoices.LUNCH, MealTypeChoices.SNACK]).delete()
